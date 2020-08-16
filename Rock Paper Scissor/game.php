c6ecc003960d427ea57a125ec029e7be473b62d3
Indrasena Kalyanam <47857838+Indrasena8@users.noreply.github.com>
2020-08-16 20:49:39 +0530

    Add files via upload

---
+++ Rock Paper Scissor/game.php
@@ -0,0 +1,104 @@
+<?php
+
+// Demand a GET parameter
+if ( ! isset($_GET['name']) || strlen($_GET['name']) < 1  ) {
+    die('Name parameter missing');
+}
+
+// If the user requested logout go back to index.php
+if ( isset($_POST['logout']) ) {
+    header('Location: index.php');
+    return;
+}
+
+// Set up the values for the game...
+// 0 is Rock, 1 is Paper, and 2 is Scissors
+$names = array('Rock', 'Paper', 'Scissors');
+$human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
+
+$computer = rand(0,2); // Hard code the computer to rock
+// TODO: Make the computer be random
+// $computer = rand(0,2);
+
+// This function takes as its input the computer and human play
+// and returns "Tie", "You Lose", "You Win" depending on play
+// where "You" is the human being addressed by the computer
+function check($computer, $human) {
+    // For now this is a rock-savant checking function
+    // TODO: Fix this
+    if ( $human == $computer ) {
+        return "Tie";
+    } else if ( $human == 1 AND $computer == 0 ){
+        return "You Win";
+    } else if ( $human == 2 AND $computer == 1 ){
+        return "You Win";
+    } else if ( $human == 0 AND $computer == 2 ){
+    	return "You Win";
+    } else if ( $human == 2  and $computer == 0 ) {
+        return "You Lose";
+    } else if ( $human == 1  and $computer == 2) {
+        return "You Lose";
+    } else if ( $human == 0  and $computer == 1 ) {
+        return "You Lose";
+    }
+    
+    return false;
+}
+
+// Check to see how the play happenned
+$result = check($computer, $human);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Indrasena Kalyanam</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Custom styles for this template -->
+<link href="starter-template.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<h1>Rock Paper Scissors</h1>
+<?php
+if ( isset($_REQUEST['name']) ) {
+    echo "<p>Welcome: ";
+    echo htmlentities($_REQUEST['name']);
+    echo "</p>\n";
+}
+?>
+<form method="post">
+<select name="human">
+<option value="-1">Select</option>
+<option value="0">Rock</option>
+<option value="1">Paper</option>
+<option value="2">Scissors</option>
+<option value="3">Test</option>
+</select>
+<input type="submit" value="Play">
+<input type="submit" name="logout" value="Logout">
+</form>
+
+<pre>
+<?php
+if ( $human == -1 ) {
+    print "Please select a strategy and press Play.\n";
+} else if ( $human == 3 ) {
+    for($c=0;$c<3;$c++) {
+        for($h=0;$h<3;$h++) {
+            $r = check($c, $h);
+            print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
+        }
+    }
+} else {
+    print "Your Play=$names[$human] Computer Play=$names[$computer] Result=$result\n";
+}
+?>
+</pre>
+</div>
+</body>
+</html>
---
+++ Rock Paper Scissor/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Indrasena Kalyanam</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Custom styles for this template -->
+<link href="starter-template.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
